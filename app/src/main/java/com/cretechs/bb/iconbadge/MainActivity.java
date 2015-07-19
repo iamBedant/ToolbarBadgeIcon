32c392f97e3af7d1387e87edb4d43cb18c6b8bd6
Bedant <bebi2527@live.in>
2015-07-19 21:53:14 +0530

    First Commit

---
+++ app/src/main/java/com/cretechs/bb/iconbadge/MainActivity.java
@@ -0,0 +1,73 @@
+package com.cretechs.bb.iconbadge;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.mikepenz.actionitembadge.library.ActionItemBadge;
+import com.mikepenz.iconics.typeface.FontAwesome;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    int badgeCount = 10;
+    Context context;
+    Button button;
+    Menu menu1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+        button = (Button) findViewById(R.id.button);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateItem(3);
+            }
+        });
+    }
+
+    private  void updateItem(int a){
+        ActionItemBadge.update(MainActivity.this,menu1.findItem(R.id.item_samplebadge),FontAwesome.Icon.faw_cart_plus,a);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu1 = menu;
+        if (badgeCount > 0) {
+            ActionItemBadge.update(this, menu.findItem(R.id.item_samplebadge),FontAwesome.Icon.faw_cart_arrow_down, badgeCount);
+           // ActionItemBadge.update(this, menu.findItem(R.id.item_samplebadge), FontAwesome.Icon.faw_android, ActionItemBadge.BadgeStyles.DARK_GREY, badgeCount);
+        } else {
+            ActionItemBadge.hide(menu.findItem(R.id.item_samplebadge));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
